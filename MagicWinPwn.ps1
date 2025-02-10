fdc2858579cf2f2e69012b008f77d5fc0d78350b
Mag1cByt3s <ppeinecke@protonmail.com>
2025-02-10 15:22:56 +0100

    update function Get-UserInfo: try to fix proper alignment

--- MagicWinPwn.ps1
+++ MagicWinPwn.ps1
@@ -72,11 +72,11 @@ function Get-UserInfo {
     Write-Host "`n[+] Assigned Privileges:" -ForegroundColor Green
     $privileges = whoami /priv | ForEach-Object { $_ -replace "\s{2,}", " | " }  # Replace multiple spaces with a separator
     if ($privileges -match "Privilege Name") {
-        Write-Host "`n    Privilege Name                    | Description                                     | State" -ForegroundColor Cyan
-        Write-Host "    --------------------------------------------------------------------------" -ForegroundColor Cyan
+        Write-Host "`n    Privilege Name                    | Description                                      | State" -ForegroundColor Cyan
+        Write-Host "    -----------------------------------------------------------------------------------" -ForegroundColor Cyan
         $privileges | Select-Object -Skip 1 | ForEach-Object { 
             if ($_ -match "(Se\S+)\s+\|\s+(.+?)\s+\|\s+(Enabled|Disabled)") {
-                Write-Host ("    {0,-35} | {1,-50} | {2}" -f $matches[1], $matches[2], $matches[3]) 
+                Write-Host ("    {0,-32} | {1,-48} | {2}" -f $matches[1], $matches[2], $matches[3]) 
             }
         }
     } else {
